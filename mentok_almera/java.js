af813345156ccabd97e797af10913c88bd17d75d
almeramentok <almerageementok@gmail.com>
2023-11-13 19:39:05 +0800

    Add comment sort

--- mentok_almera/java.js
+++ mentok_almera/java.js
@@ -1,34 +1,66 @@
-let commenterName = document.getElementById('commenter_name');
-let commenterText = document.getElementById('comment_text');
-let commentButton = document.getElementById('comment_button');
-let commentList = document.getElementById('data_list');
+let commenterName = document.getElementById("commenter_name");
+let commenterText = document.getElementById("comment_text");
+let commentButton = document.getElementById("comment_button");
+let commentList = document.getElementById("data_list");
+let commentsArray = [];
 
-commenterName.addEventListener('input', validateForm);
-commenterText.addEventListener('input', validateForm);
-commentButton.addEventListener('click', addComment);
+commenterName.addEventListener("input", validateForm);
+commenterText.addEventListener("input", validateForm);
+commentButton.addEventListener("click", addComment);
 
 function validateForm() {
-    let nameValue = commenterName.value.trim();
-    let commentValue = commenterText.value.trim();
+  let nameValue = commenterName.value.trim();
+  let commentValue = commenterText.value.trim();
 
-    if (nameValue && commentValue) {
-        commentButton.disabled = false;
-    } else {
-        commentButton.disabled = true;
-    }
+  if (nameValue && commentValue) {
+    commentButton.disabled = false;
+  } else {
+    commentButton.disabled = true;
+  }
 }
 
 function addComment() {
-    let addName = commenterName.value.trim();
-    let addComment = commenterText.value.trim();
+  let addName = commenterName.value.trim();
+  let addCommentText = commenterText.value.trim();
 
-    if (addName && addComment) {
-        let commentItem = document.createElement("p");
-        commentItem.textContent = `${addName} - ${addComment}`;
-        commentList.appendChild(commentItem);
+  if (addName && addCommentText) {
+    let currentDate = new Date();
+    let formattedDate = currentDate.toISOString();
 
-        commenterName.value = ""; 
-        commenterText.value = "";
-        commentButton.disabled = true; 
-    }
+    let commentItem = {
+      name: addName,
+      comment: addCommentText,
+      date: formattedDate,
+    };
+
+    commentsArray.push(commentItem);
+
+    commentsArray.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    displayComments();
+
+    commenterName.value = "";
+    commenterText.value = "";
+    commentButton.disabled = true;
+  }
+}
+
+function displayComments() {
+  commentList.innerHTML = "";
+
+  commentsArray.forEach((comment) => {
+    let commentItem = document.createElement("p");
+    commentItem.textContent = `${comment.name} - ${comment.comment} - (${comment.date})`;
+    commentList.appendChild(commentItem);
+  });
+}
+
+function sortCommentsAscending() {
+  commentsArray.sort((a, b) => new Date(a.date) - new Date(b.date));
+  displayComments();
+}
+
+function sortCommentsDescending() {
+  commentsArray.sort((a, b) => new Date(b.date) - new Date(a.date));
+  displayComments();
 }
